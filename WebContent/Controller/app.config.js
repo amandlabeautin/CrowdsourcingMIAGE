596ca8c2f2ef0a3976ce13625bcbf573a1882ae1
amandlabeautin <amandlabeautin@gmail.com>
2017-05-20 19:55:09 +0200

    Affichage des pages en fonction du rôle du user

--- WebContent/Controller/app.config.js
+++ WebContent/Controller/app.config.js
@@ -1,44 +1,46 @@
 angular
     .module('app')
-	.config(function($routeProvider, $locationProvider) {
+	.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 		$locationProvider.hashPrefix('');
-		$routeProvider
-			.when('/home', {
-				templateUrl: 'View/home.view.html',
-				controller : 'homeCtrl'
-			})
-			.when('/', {
-				templateUrl: 'View/signin.view.html',
-				controller : 'SigninCtrl',
-				access: 'public'
-			})
-			.when('/register', {
-				templateUrl: 'View/register.view.html',
-				controller : 'RegisterCtrl',
-				access: 'public'
-			})
-			.when('/admin', {
-				templateUrl: 'View/admin.view.html',
-				controller : 'adminCtrl',
-				access: 'admin'
-			})
-			.when('/addUser', {
-				templateUrl: 'View/addUser.view.html',
-				controller : 'addUserCtrl',
-				access: 'admin'
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
+		$stateProvider
+			.state('signin', {
+                url: '/',
+                templateUrl: '../View/signin.view.html',
+                controller: 'SigninCtrl',
+                access: 'public'
+	        })
+	        .state('home', {
+                url: '/home',
+                templateUrl: '../View/home.view.html',
+                controller: 'homeCtrl'
+	        })
+	        .state('register', {
+                url: '/register',
+                templateUrl: '../View/register.view.html',
+                controller: 'RegisterCtrl',
+                access: 'public'
+	        })
+	        .state('admin', {
+                url: '/admin',
+                templateUrl: '../View/signin.view.html',
+                controller: 'SigninCtrl',
+                access: 'public'
+	        })
+	        .state('addUser', {
+                url: '/addUser',
+                templateUrl: '../View/addUser.view.html',
+                controller: 'addUserCtrl'
+	        });
+	    $urlRouterProvider.otherwise("/");
 	})
 	.run(['$rootScope', '$state', '$timeout','UserService','UtilService',
 	    function ($rootScope, $location, $timeout, UserService, UtilService) {
 		
 			$rootScope.$on('$stateChangeStart', function(evt, toState, toParams, fromState, fromParams) {
-				//console.log(toState);
-		    	//console.log(toState.name);
+				console.log(toState);
+		    	console.log(toState.name);
 				var access = toState.access;
-				//console.log(toState.access);
+				console.log(toState.access);
 		    	if(access != 'public'){
 		    		if(UserService.isUserLoggedIn()){
 		    			$rootScope.currentNavLink=toState.name;
